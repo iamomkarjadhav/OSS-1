7dd05cbda8eb33f155476ca0effd1ee77ee26334
RavindraMohite <108930810+RavindraMohite@users.noreply.github.com>
2022-07-08 15:59:51 +0530

    first Commit
    this changes have been wriitten for initiating system

---
+++ HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TFLWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int currentVisitCount = int.Parse(this.HttpContext.Session["visits"].ToString());
+
+            this.HttpContext.Session["visits"] = currentVisitCount + 1;
+                
+            return View();
+        }
+
+
+        public ActionResult Aboutus()
+        {
+            int currentVisitCount = int.Parse(this.HttpContext.Session["visits"].ToString());
+            this.HttpContext.Session["visits"] = currentVisitCount + 1;
+            return View();
+        }
+    }
+}
